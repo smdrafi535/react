be6ec429c3784b86c5d6d7497445275a787daaf6
Mahammad Rafi Shaik <36479681+smdrafi535@users.noreply.github.com>
2023-10-23 13:27:04 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from './components/Navbar.js';
+import Hero from './components/Hero.js';
+import About from './components/About.js';
+import Services from './components/Services.js';
+import Tours from './components/Tours.js';
+import Footer from './components/Footer.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours/>
+      <Footer />
+    </>
   );
 }
 
